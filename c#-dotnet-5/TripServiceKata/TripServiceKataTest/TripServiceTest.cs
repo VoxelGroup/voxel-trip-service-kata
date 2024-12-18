1de89339f0d2b72198697feb4c312508a23b4d26
jruizvoxel <jruiz@voxelgroup.net>
2024-12-18 15:48:42 +0100

    TEST: ShouldFailsIfTheUserIsNotLoggedIn

--- c#-dotnet-5/TripServiceKata/TripServiceKataTest/TripServiceTest.cs
+++ c#-dotnet-5/TripServiceKata/TripServiceKataTest/TripServiceTest.cs
@@ -1,14 +1,29 @@
+using System;
 using Xunit;
 using FluentAssertions;
+using TripServiceKata.Exception;
+using TripServiceKata.Trip;
 
 namespace TripServiceKata.Tests
 {
     public class TripServiceTest
     {
         [Fact]
-        public void Foo()
+        public void ShouldFailsIfTheUserIsNotLoggedIn()
         {
-            true.Should().Be(false);
+            var tripService = new TestableTripService();
+
+            Action act = () => tripService.GetTripsByUser(null);
+
+            act.Should().Throw<UserNotLoggedInException>();
+        }
+    }
+
+    public class TestableTripService : TripService
+    {
+        protected override User.User GetLoggedUser()
+        {
+            return null;
         }
     }
 }
